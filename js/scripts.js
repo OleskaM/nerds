bfc4474269e0b2cc31e69b8454778e41c08734df
Olesya Murga <lesja@murga.kiev.ua>
2015-11-17 01:20:43 +0200

    scripts fixed: popup close, animation JS in a separate file

---
+++ js/scripts.js
@@ -0,0 +1,74 @@
+var link = document.querySelector(".contact-us");
+var popup = document.querySelector(".modal-content");
+var close = popup.querySelector(".modal-content-close");
+var close2 = popup.querySelector(".cancel");
+var login = popup.querySelector("#user-name");
+var email = popup.querySelector("#user-email");
+var message = popup.querySelector("#user-message");
+var form = popup.querySelector("form");
+var storage = localStorage.getItem("login");
+
+link.addEventListener("click", function(event) {
+    event.preventDefault();
+    popup.classList.add("modal-content-show");
+
+    if (storage) {
+        login.value = storage;
+        email.focus();
+    } else {
+        login.focus();
+    }
+});
+
+close.addEventListener("click", function(event) {
+    event.preventDefault();
+    popup.classList.remove("modal-content-show");
+    popup.classList.remove("modal-error");
+});
+
+close2.addEventListener("click", function(event) {
+    event.preventDefault();
+    popup.classList.remove("modal-content-show");
+    popup.classList.remove("modal-error");
+});
+
+
+form.addEventListener("submit", function(event) {
+    if (!login.value || !email.value || !message.value) {
+        event.preventDefault();
+        popup.classList.remove("modal-error");
+        popup.offsetWidth = popup.offsetWidth;
+        popup.classList.add("modal-error");
+    } else {
+        localStorage.setItem("login", login.value);
+    }
+});
+
+window.addEventListener("keydown", function(event) {
+    if (event.keyCode === 27) {
+        if (popup.classList.contains("modal-content-show")) {
+            popup.classList.remove("modal-content-show");
+            popup.classList.remove("modal-error");
+        }
+    }
+});
+
+
+ymaps.ready(function() {
+    var myMap = new ymaps.Map('map', {
+            center: [59.938723, 30.323094],
+            zoom: 16
+        }),
+        myPlacemark = new ymaps.Placemark([59.938725, 30.323132],
+            {
+                hintContent: 'Nёrds'
+            }, {
+                iconLayout: 'default#image',
+                iconImageHref: 'img/baloon.png',
+                iconImageSize: [231, 190],
+                iconImageOffset: [-49, -195]
+            });
+
+    myMap.geoObjects.add(myPlacemark);
+});
+
